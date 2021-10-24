87c70faffc02eca8cc151fc2dc4fb17ab69635e7
Prakhar srivastav <prakhar.20208089@mnnit.ac.in>
2021-10-24 12:32:02 +0530

    add 3 number

---
+++ add copy.cpp
@@ -0,0 +1,12 @@
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main(){
+    int num1;
+    int num2;
+    int num3;
+    cin>>num1>>num2>>num3;
+    cout<<(num1+num2+num3);
+}
